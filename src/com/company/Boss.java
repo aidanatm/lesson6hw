83a4cdcdeaecbfc9e619ee365abf09d0006a6fed
aidanatm <temirkulovaa2005@gmail.com>
2020-11-26 16:45:18 +0600

    Initial commit

---
+++ src/com/company/Boss.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Boss extends GameEntity{
+    Weapon weapon;
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    @Override
+    public String toString() {
+        return "Boss{" +
+                "weapon=" + weapon +
+                ", health=" + health +
+                ", damage=" + damage +
+                '}';
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+
+    package com.company;
+
+import java.util.Random;
+
+        public class Main {
+         public static void main(String[] args){
+             Weapon weapon = new Weapon();
+
+             weapon.setType("Avtomat");
+
+             weapon.setName("AK-47");
+
+             Boss boss = new Boss();
+
+             boss.setHealth(500);
+
+             boss.setDamage(50);
+
+             boss.setWeapon(weapon);
+
+            
+
+             System.out.println(weapon.toString() + boss.toString());
+         }
+
+        }
+
+
+
